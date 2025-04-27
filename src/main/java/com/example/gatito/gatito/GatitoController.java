1cccf6c5bca4c7751bb4b4f8dd5259d990f422c8
maffzz <maria.lazon@utec.ued.pe>
2025-04-27 16:17:40 -0500

    dtos (missing error handling)

--- src/main/java/com/example/gatito/gatito/GatitoController.java
+++ src/main/java/com/example/gatito/gatito/GatitoController.java
@@ -1,7 +1,11 @@
 
 package com.example.gatito.gatito;
 
+import com.example.gatito.gatito.dto.CreateGatitoDTO;
+import com.example.gatito.gatito.dto.UpdateGatitoDTO;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,61 +17,57 @@ public class GatitoController {
     private final GatitoService gatitoService;
 
     @GetMapping("/todos")
-    public List<Gatito> getGatitos() {
-        return gatitoService.getGatitos();}
+    public ResponseEntity<List<Gatito>> getGatitos() {
+        return ResponseEntity.ok(gatitoService.getGatitos());}
 
     @PostMapping("/crear")
-    public String createGatito(@RequestBody Gatito gatito) {
-        return gatitoService.createGatito(gatito);}
+    public ResponseEntity<String> createGatito(@Valid @RequestBody CreateGatitoDTO dtoNew) {
+        return ResponseEntity.ok(gatitoService.createGatito(dtoNew));}
 
     @PatchMapping("/actualizar/{Id}")
-    public String updateGatito(@PathVariable Long Id, @RequestBody Gatito gatitoActu) {
-        return gatitoService.updateGatito(Id, gatitoActu);}
+    public ResponseEntity<String> updateGatito(@PathVariable Long Id, @Valid @RequestBody UpdateGatitoDTO dtoActu) {
+        return ResponseEntity.ok(gatitoService.updateGatito(Id, dtoActu));}
 
     @DeleteMapping("/eliminar/{Id}")
-    public String deleteGatito(@PathVariable Long Id) {
-        return gatitoService.deleteGatito(Id);}
+    public ResponseEntity<String> deleteGatito(@PathVariable Long Id) {
+        return ResponseEntity.ok(gatitoService.deleteGatito(Id));}
 
-    @GetMapping("/{Id}")
-    public Gatito getGatitoById(@PathVariable Long Id) {
-        return gatitoService.getGatitoById(Id);}
+    @GetMapping("/id/{Id}")
+    public ResponseEntity<Gatito> getGatitoById(@PathVariable Long Id) {
+        return ResponseEntity.ok(gatitoService.getGatitoById(Id));}
 
     @GetMapping("/nombre/{nombre}")
-    public List<Gatito> getGatitoByNombre(@PathVariable String nombre) {
-        return gatitoService.getGatitoByNombre(nombre);}
+    public ResponseEntity<List<Gatito>> getGatitoByNombre(@PathVariable String nombre) {
+        return ResponseEntity.ok(gatitoService.getGatitoByNombre(nombre));}
 
     @GetMapping("/color/{color}")
-    public List<Gatito> getGatitoByColor(@PathVariable String color) {
-        return gatitoService.getGatitoByColor(color);}
+    public ResponseEntity<List<Gatito>> getGatitoByColor(@PathVariable String color) {
+        return ResponseEntity.ok(gatitoService.getGatitoByColor(color));}
 
     @GetMapping("/genero/{genero}")
-    public List<Gatito> getGatitoByGenero(@PathVariable String genero) {
-        return gatitoService.getGatitoByGenero(genero);}
-
-    @GetMapping("/nacimiento/{nacimiento}")
-    public List<Gatito> getGatitoByNacimiento(@PathVariable Integer nacimiento) {
-        return gatitoService.getGatitoByNacimiento(nacimiento);}
+    public ResponseEntity<List<Gatito>> getGatitoByGenero(@PathVariable String genero) {
+        return ResponseEntity.ok(gatitoService.getGatitoByGenero(genero));}
 
     @GetMapping("/series/{Id}")
-    public List<String> getSeries(@PathVariable Long Id) {
-        return gatitoService.getSeries(Id);}
+    public ResponseEntity<List<String>> getSeries(@PathVariable Long Id) {
+        return ResponseEntity.ok(gatitoService.getSeries(Id));}
 
     @GetMapping("/canciones/{Id}")
-    public List<String> getCanciones(@PathVariable Long Id) {
-        return gatitoService.getCanciones(Id);}
+    public ResponseEntity<List<String>> getCanciones(@PathVariable Long Id) {
+        return ResponseEntity.ok(gatitoService.getCanciones(Id));}
 
     @PostMapping("/agregarserie/{Id}")
-    public String agregarSerie(@PathVariable Long Id, @RequestBody String newSerie) {
-        return gatitoService.agregarSerie(Id, newSerie);}
+    public ResponseEntity<String> agregarSerie(@PathVariable Long Id, @RequestBody String newSerie) {
+        return ResponseEntity.ok(gatitoService.agregarSerie(Id, newSerie));}
 
     @PostMapping("/eliminarserie/{Id}")
-    public String eliminarSerie(@PathVariable Long Id, @RequestBody String noSerie) {
-        return gatitoService.eliminarSerie(Id, noSerie);}
+    public ResponseEntity<String> eliminarSerie(@PathVariable Long Id, @RequestBody String noSerie) {
+        return ResponseEntity.ok(gatitoService.eliminarSerie(Id, noSerie));}
 
     @PostMapping("/agregarcancion/{Id}")
-    public String agregarCancion(@PathVariable Long Id, @RequestBody String newCancion) {
-        return gatitoService.agregarCancion(Id, newCancion);}
+    public ResponseEntity<String> agregarCancion(@PathVariable Long Id, @RequestBody String newCancion) {
+        return ResponseEntity.ok(gatitoService.agregarCancion(Id, newCancion));}
 
     @PostMapping("/eliminarcancion/{Id}")
-    public String eliminarCancion(@PathVariable Long Id, @RequestBody String noCancion) {
-        return gatitoService.eliminarCancion(Id, noCancion);}}
+    public ResponseEntity<String> eliminarCancion(@PathVariable Long Id, @RequestBody String noCancion) {
+        return ResponseEntity.ok(gatitoService.eliminarCancion(Id, noCancion));}}
--- src/main/java/com/example/gatito/gatito/GatitoRepository.java
+++ src/main/java/com/example/gatito/gatito/GatitoRepository.java
@@ -8,5 +8,4 @@ import java.util.List;
 public interface GatitoRepository extends JpaRepository<Gatito, Long> {
     List<Gatito> findByNombre(String nombre);
     List<Gatito> findByColor(String color);
-    List<Gatito> findByGenero(String genero);
-    List<Gatito> findByNacimiento(Integer nacimiento);}
+    List<Gatito> findByGenero(String genero);}
